58efd7ccbaee8a5a103a0b75c639318922b71fbf
Oddernumse <72282154+Oddernumse@users.noreply.github.com>
2021-01-06 10:36:36 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,29 +1,49 @@
+/*function gameData() {
+    this.money = 100000;
+    this.moneyPerClick = 1;
+    this.upgradeCost = 10;
+    this.upgradeCost2 = 100;
+    this.upgradeCost3 = 15;
+    this.clickTime = 1000;
+    this.autoClickers = 1
+    this.moneyPerSec = 1;
+    this.mps = 0.1;
+}*/
+
 var gameData = {
-    money: 0,
+    money: 1000000,
     moneyPerClick: 1,
     upgradeCost: 10,
     upgradeCost2: 100,
-    upgradeCost3: 150,
+    upgradeCost3: 15,
     clickTime: 1000,
-    autoClickers: 0
+    //autoClickers: 1,
+    moneyPerSec: 1,
+    mps: 0.1
 }
 
+//let mps = 0.1;
+//mps = 
+//var timer = window.setInterval(function(){autoClickFunc2()}, 1000);
 
-//This currently just constantly increases money by moneyPerClick once every second. Make this an upgrade
-
-//TRY TO MAKE AUTOCLICKFUNC A FUNCTION STORED IN A VARIABLE. Alright that wont actually change much, so find some other way to decrease clickTime
-
-function autoClickFunc() {
-    var upgradeIncrease = (70/100) * gameData.upgradeCost3;
-    if (money >= gameData.upgradeCost3) {
-        gameData.upgradeCost3 += upgradeIncrease;
-        window.setInterval(function() {
-        clickFunction()
-        }, this.clickTime);
-        autoClickers++;
-       gameData.upgradeCost3 = Math.round(gameData.upgradeCost3)
+//This function
+function autoClickFunc1() {
+    if (gameData.money >= gameData.upgradeCost3) {
+        gameData.money -= gameData.upgradeCost3
+        gameData.mps += 0.1;
     }
 }
+
+function prettify(input){
+    var output = Math.round(input * 1000000)/1000000;
+	return output;
+}
+
+function autoClickFunc2() {
+    window.setInterval(function() { 
+    gameData.money = gameData.money + gameData.mps}, 1000);
+ 
+} 
 
 //This function increases money by whatever moneyPerClick is, everytime you press the button
 function clickFunction() {
@@ -54,35 +74,47 @@ function increaseClickMoney() {
 
 //This updates some values every 50 milliseconds
 function updater() {
-    document.getElementById("output").innerHTML = "You have $" + gameData.money;
+    document.getElementById("output").innerHTML = "You've typed " + prettify(gameData.money) + " Lines";
     document.getElementById("test").innerHTML = "Upgrade Cost: " + gameData.upgradeCost + " " + gameData.upgradeCost3// + " " + gameData.upgradeCost3;
-    document.getElementById("autoClick").innerHTML = "Autoclickers: " + gameData.autoClickers;
+    document.getElementById("autoClick").innerHTML = "LpS: " + prettify(gameData.mps);
 }
 
 setInterval(updater, 50);
 
 var saveGameLoop = window.setInterval(function() {
     localStorage.setItem("moneySave", JSON.stringify(gameData))
-}, 15)
+}, 15000)
 
 var saveGame = JSON.parse(localStorage.getItem("moneySave"))
 if (saveGame !== null) {
     gameData = saveGame
 }
-/* function saveGame() {
-    try {
-        localStorage.setItem('copperSave', JSON.stringify(gameData));
-    } catch(err) {
-        console.log("cannot access localStorage - browser may be old or storage may be corrupt");
-    }
-    console.log("Game saved successfully");
+
+//This right here is the world's shittiest way of wiping a save. It doesn't even wipe it, it just resets the values to what they originally were through mediocre means
+function wipeSave() {
+    localStorage.removeItem("moneySave");
+    gameData.money -= gameData.money;
+    gameData.moneyPerClick -= gameData.moneyPerClick;
+    gameData.moneyPerClick += 1;
+    gameData.upgradeCost -= gameData.upgradeCost
+    gameData.upgradeCost += 10;
+    gameData.upgradeCost2 -= gameData.upgradeCost2;
+    gameData.upgradeCost2 += 100;
+    gameData.upgradeCost3 -= gameData.upgradeCost3;
+    gameData.upgradeCost3 += 15;
+    gameData.autoClickers -= gameData.autoClickers;
 }
 
-function loadGame() {
-    var gameLoad = JSON.parse(localStorage.getItem("copperSave"));
-} */
-//currently the upgradeIncrease doesn't work properly. Find some way to increase upgradeCost proportionatly to moneyPerCLick
+window.onload = autoClickFunc2();
 
-//Todo: fix decreasing the buy time on autoBuy. Implement saving(cookie clicker had a link to one, but make one yourself if you can)
+/*     var upgradeIncrease = (70/100) * gameData.upgradeCost3;
+    if (gameData.money >= gameData.upgradeCost3) {
+        gameData.upgradeCost3 += upgradeIncrease;
+        window.setInterval(function() {
+        clickFunction()
+        }, gameData.clickTime);
+        gameData.autoClickers++;
+        gameData.upgradeCost3 = Math.round(gameData.upgradeCost3)
+    } */
 
-//FIX THE AUTOBUY. CURRENT IDEA IS TO MAKE IT A FUNCTION SINCE THAT RUNS AND UPDATES ITS VALUES EVERYTIME ITS RUN. IF ANY VARIABLES ARE NEEDED MAKE THEM INSIDE THE FUNCTION
+    //ask GreyGoose about the whole not being able to access mps in gameData thing
