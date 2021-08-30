c12d839c6e88faa5f40e4755f4911e0154c0231c
Oddernumse <72282154+Oddernumse@users.noreply.github.com>
2021-08-30 18:35:18 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,89 +1,7 @@
 //started working on this december 21st 2020
-//today: february 5nd 2021
+//today: february 8th 2021
 
-import {multiplier, gameData} from "./module.js";
-
-var stop = false;
-var frameCount = 0;
-//var $results = $("#results");
-var fps, fpsInterval, startTime, now, then, elapsed;
-
-startAnimating(30);
-
-function startAnimating(fps) {
-    fpsInterval = 1000 / fps;
-    then = Date.now();
-    startTime = then;
-    console.log(startTime);
-    animate();
-}
-
-function animate() {
-
-    // stop
-    if (stop) {
-        return;
-    }
-
-    // request another frame
-
-    requestAnimationFrame(animate);
-
-    // calc elapsed time since last loop
-
-    now = Date.now();
-    elapsed = now - then;
-
-    // if enough time has elapsed, draw the next frame
-
-    if (elapsed > fpsInterval) {
-
-        // Get ready for next frame by setting then=now, but...
-        // Also, adjust for fpsInterval not being multiple of 16.67
-        then = now - (elapsed % fpsInterval);
-
-        // draw stuff here
-
-
-        // TESTING...Report #seconds since start and achieved fps.
-        var sinceStart = now - startTime;
-        var currentFps = Math.round(1000 / (sinceStart / ++frameCount) * 100) / 100;
-        //$results.text("Elapsed time= " + Math.round(sinceStart / 1000 * 100) / 100 + " secs @ " + currentFps + " fps.");
-
-    }
-}
-
-/* 
-============================================================================================================================================================================================================================
-THE CODE ABOVE ONLY EXISTS TO LIMIT THE FPS AT WHICH THE THINGS DRAWN IN THE SPECIFIED SECTION WILL RUN. IT SHOULD NOT BE USED FOR LITERALLY ANYTHING ELSE. I WILL ONLY PUT THINGS IN THERE THAT ABSOLUTELY NEED TO BE THERE
-============================================================================================================================================================================================================================
-*/
-
-
-//export { multiplier /*or gamedata whichever works*/};
-
-let w;
-if (typeof (w) == "undefined") {
-    w = new Worker("webworkers.js", { type: "module" });
-}
-
-w.onmessage = function (event) {
-    document.getElementById("result").innerHTML = event.data;
-}
-
-var numberThing = 0;
-
-/*var test ={
-    monkeyWriterValue: 0.1
-}*/
-
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0"
-}
+const { gameData } = require('./module.mjs');
 
 //Use this function on any number with rogue decimals
 function prettify(input) {
@@ -98,7 +16,7 @@ function updater() {
     document.getElementById("output2").innerHTML = "Monkey Writer: " + fixedNum + " Trainees";
     document.getElementById("output5").innerHTML = "Monkey Trainer: " + fixedNum2 + " Lines";
     document.getElementById("autoClick").innerHTML = "LpS: " + prettify(gameData.mps);
-    //document.getElementById("output3").innerHTML = gameData.trainees + " Trainees" + " Trainee Increase: " + gameData.traineeIncrease;
+    document.getElementById("output3").innerHTML = gameData.trainees + " Trainees" + " Trainee Increase: " + gameData.traineeIncrease;
 }
 setInterval(updater, 50);
 window.onload = updater;
@@ -142,7 +60,7 @@ function monkeyEmployerFunc2() {
 
 let i = 0;
 
-var saveGameLoop = window.setInterval(function () {
+/*var saveGameLoop = window.setInterval(function () {
     localStorage.setItem("moneySave", JSON.stringify(gameData))
     console.log("saved");
 }, 15000)
@@ -150,7 +68,7 @@ var saveGameLoop = window.setInterval(function () {
 var saveGame = JSON.parse(localStorage.getItem("moneySave"))
 if (saveGame !== null) {
     gameData = saveGame
-}
+}*/
 
 //This right here is the world's shittiest way of wiping a save. It doesn't even wipe it, it just resets the values to what they originally were through mediocre means
 function wipeSave() {
@@ -169,28 +87,4 @@ function wipeSave() {
     gameData.trainees += 30;
     gameData.mps += 0.1;
     //gameData.mps += 1;
-}
-
-/*
-==============================================
-IF YOU WANNA EDIT THE TABS CODE ITS BELOW THIS
-==============================================
-*/
-
-function openTab(evt, tabName) {
-    var i, tabContent, tabLinks;
-
-
-    tabContent = document.getElementsByClassName("tabContent");
-    for (i = 0; i < tabContent.length; i++) {
-        tabContent[i].style.display = "none";
-    }
-
-    tabLinks = document.getElementsByClassName("tabLinks");
-    for (i = 0; i < tabLinks.length; i++) {
-        tabLinks[i].className = tabLinks[i].className.replace(" active", "");
-    }
-
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
 }
--- webworkers.js
+++ webworkers.js
@@ -1,18 +1,19 @@
-import {multiplier, gameData} from "./module.js"
+import { gameData, multiplier } from "./module.mjs"
 
 function tester() {
-    for (let i=0; i < 100; i++) {
+    for (let i=0; i < 10; i++) {    
+        setInterval(function(){
         var fixedNum = gameData.trainees.toFixed(2);
         var fixedLines = gameData.lines.toFixed(2);
-        var test = gameData.lines += gameData.mps / multiplier / 30;
-        var test2 = gameData.trainees += gameData.traineeIncrease / multiplier / 30;
+        var test = gameData.lines += gameData.mps / multiplier / 100;
+        var test2 = gameData.trainees += gameData.traineeIncrease / multiplier / 100;
 
-        postMessage("weed");
+        postMessage(fixedLines)
         /*document.getElementById("output3").innerHTML = fixedNum + " trainees";
         document.getElementById("output4").innerHTML = fixedLines + " Lines";
-        //monkeyEmployerFunc();*/
-        setTimeout(tester,500);
+        monkeyEmployerFunc();*/
+        }, 10);
     }
 }
 
-tester() 
+tester()
